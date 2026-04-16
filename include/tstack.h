654cef0df9016131d5b811ef3ac8b77ffc2923b3
morosArtem <morosart1995@gmail.com>
2026-04-16 22:53:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T arr[size];
+  int top_ind;
+
+ public:
+  TStack() : top_ind(-1) {}
+
+  void push(T value) {
+    if (top_ind < size - 1) {
+      arr[++top_ind] = value;
+    }
+  }
+
+  T pop() {
+    if (top_ind >= 0) {
+      return arr[top_ind--];
+    }
+    return T();
+  }
+
+  T top() const {
+    if (top_ind >= 0) {
+      return arr[top_ind];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return top_ind == -1;
+  }
+
+  bool isFull() const {
+    return top_ind == size - 1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
